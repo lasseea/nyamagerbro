c3b5957e4cc638bc4d6212cac4f7fd64d0ae7324
Lasse Albrechtsen <leadk90@gmail.com>
2016-11-23 01:12:01 +0100

    Admin auth
    addition of admin column in users table, kernel registration of new
    middleware and example use of it for a route in web.php. Tested to work
    as intended

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'admin',
     ];
 
     /**
@@ -26,4 +26,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value); //this is used for password hashing
+    }
+
+    public function isAdmin()
+    {
+        return $this->admin; //this is used for admin auth
+    }
 }
