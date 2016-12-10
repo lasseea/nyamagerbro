46d18adada1e61693fa0ec7ab848548014cc771e
Lasse Albrechtsen <leadk90@gmail.com>
2016-12-10 18:22:47 +0100

    added font awesome for scalable icons
    font awesome contains css and icon files that are easy to integrate. The
    facebook icon will be of use

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
     <link href="/css/custom.css" rel="stylesheet">
+    <link href="/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
     <!-- Scripts -->
     <script>
