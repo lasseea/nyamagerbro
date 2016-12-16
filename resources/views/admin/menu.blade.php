283fbe3641a2460af9011252d7c23779f8aec6df
Lasse Albrechtsen <leadk90@gmail.com>
2016-12-16 01:35:04 +0100

    added admin panel to main admin page
    added admin panel with link buttons to all of the admin pages

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -6,6 +6,27 @@
 
 @section('content')
     <div class="container">
-        Admin
+        <h2>Admin panel</h2>
+        <br>
+        <div class="list-group">
+            <a href="#" class="list-group-item active">
+                Tilføj:
+            </a>
+            <a href="#" class="list-group-item list-group-item-action">Butik</a>
+            <a href="#" class="list-group-item list-group-item-action">Nyhed/Event</a>
+            <a href="#" class="list-group-item list-group-item-action">Jobopslag</a>
+            <a href="#" class="list-group-item list-group-item-action">Lokaleudlejning</a>
+            <a href="#" class="list-group-item list-group-item-action">Billede/Logo</a>
+        </div>
+        <div class="list-group">
+            <a href="#" class="list-group-item active">
+                Opdater/Slet:
+            </a>
+            <a href="#" class="list-group-item list-group-item-action">Butik</a>
+            <a href="#" class="list-group-item list-group-item-action">Nyhed/Event</a>
+            <a href="#" class="list-group-item list-group-item-action">Jobopslag</a>
+            <a href="#" class="list-group-item list-group-item-action">Lokaleudlejning</a>
+            <a href="#" class="list-group-item list-group-item-action">Billede/Logo</a>
+        </div>
     </div>
 @endsection
