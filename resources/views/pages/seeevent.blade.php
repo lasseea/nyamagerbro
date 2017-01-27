ca106479f3ea183cefd4c3786d8d0bb7bdfa3d20
Lasse Albrechtsen <leadk90@gmail.com>
2017-01-27 03:47:53 +0100

    events views fully done
    events views fully done and tested

--- resources/views/pages/seeevent.blade.php
+++ resources/views/pages/seeevent.blade.php
@@ -9,6 +9,7 @@
         @foreach($events as $event)
             <h1><b>{{ $event->title }}</b><br></h1>
             <h3><b>{{ $event->date }}</b><br></h3>
+            <img src="{{ $event->event_img_link }}" class="large_img"></a>
             <p> {!! nl2br(e($event->full_description)) !!} </p>
         @endforeach
     </div>
